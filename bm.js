7323ced9cedaa1d0b8d2357a9b84984e6a111076
vanderMandele, Dexx <dexx.vandermandele@homeadvisor.com>
2020-11-04 10:17:34 -0700

    Move people

--- bm.js
+++ bm.js
@@ -1,24 +1,44 @@
 javascript:(function() {
-  var url = 'https://habitica.com/';
+  function getElementByXpath(xpath, parent) {
+    parent = parent || document;
+    return document.evaluate(xpath, parent, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  }
+
+  function removeLastNavItem() {
+    navItems = document.querySelectorAll('li.topbar-item.droppable');
+    if (navItems) {
+      navItems[navItems.length - 1].remove();
+    }
+  }
+
+  function removeRewards() {
+    document.querySelector('.reward').remove();
+    var cols = document.querySelectorAll('div.tasks-column');
+    for (var i = 0; i < cols.length; i++) {
+      cols[i].style.flex = '0 0 33%';
+      cols[i].style['max-width'] = '33%';
+    }
+  }
+
+  function moveMarie() {
+    const memberBar = document.querySelector('.party-members');
+    const marie = getElementByXpath('//div[contains(@class, "member-details")][.//span[text() = "MarenaVarena"]]', memberBar);
+    memberBar.appendChild(marie);
+  }
+
+  const url = 'https://habitica.com/';
   if (!document.location.href.startsWith(url)) {
     window.location.href = url;
     return;
   }
-  var navItems;
-  var interval = setInterval(() => {
-    navItems = document.querySelectorAll('li.topbar-item.droppable');
-    if (navItems) {
-      try {
-        navItems[navItems.length - 1].remove();
-        document.querySelector('.reward').remove();
-        var cols = document.querySelectorAll('div.tasks-column');
-        for (var i = 0; i < cols.length; i++) {
-          cols[i].style.flex = '0 0 33%';
-          cols[i].style['max-width'] = '33%';
-        }
-      } finally {
-        clearInterval(interval);
-      }
+
+  const interval = setInterval(() => {
+    try {
+      removeLastNavItem();
+      removeRewards();
+      moveMarie();
+    } finally {
+      clearInterval(interval);
     }
   }, 200);
 })();
